c0bb5c9fe9feea23eaed6a823100dd32f7e878a6
Martin Rodin <rodin.martin@gmail.com>
2022-11-20 19:22:31 +0100

    testing react router defer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import {
+  useLoaderData,
+  defer,
+  Await,
+  useAsyncValue,
+  useAsyncError,
+} from "react-router-dom";
+
+import "./App.css";
+
+export const loader = async () => {
+  const critical1Promise = fetch("/test?text=critical1&delay=250").then((res) =>
+    res.json()
+  );
+  const critical2Promise = fetch("/test?text=critical2&delay=500").then((res) =>
+    res.json()
+  );
+  const lazyResolvedPromise = fetch("/test?text=lazyResolved&delay=100").then(
+    (res) => res.json()
+  );
+  const lazy1Promise = fetch("/test?text=lazy1&delay=500").then((res) =>
+    res.json()
+  );
+  const lazy2Promise = fetch("/test?text=lazy2&delay=1500").then((res) =>
+    res.json()
+  );
+  const lazy3Promise = fetch("/test?text=lazy3&delay=2500").then((res) =>
+    res.json()
+  );
+  const lazyErrorPromise = fetch("/test?text=lazy3&delay=3000").then((res) => {
+    throw Error("Oh noo!");
+  });
+
+  // parallel fetch
+  return defer({
+    critical1: await critical1Promise,
+    critical2: await critical2Promise,
+    lazyResolved: lazyResolvedPromise,
+    lazy1: lazy1Promise,
+    lazy2: lazy2Promise,
+    lazy3: lazy3Promise,
+    lazyError: lazyErrorPromise,
+  });
+};
+
+function RenderAwaitedData() {
+  let data = useAsyncValue();
+  return <p>{data.text}</p>;
+}
+
+function RenderAwaitedError() {
+  let error = useAsyncError();
+  return (
+    <p style={{ color: "red" }}>
+      Error (errorElement)!
+      <br />
+      {error.message} {error.stack}
+    </p>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = useLoaderData();
+
+  // console.log(data);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{data.critical1.text}</p>
+      <p>{data.critical2.text}</p>
+
+      <React.Suspense fallback={<p>loading... should not see me!</p>}>
+        <Await resolve={data.lazyResolved}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      <React.Suspense fallback={<p>loading 1...</p>}>
+        <Await resolve={data.lazy1}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      <React.Suspense fallback={<p>loading 2...</p>}>
+        <Await resolve={data.lazy2}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      <React.Suspense fallback={<p>loading 3...</p>}>
+        <Await resolve={data.lazy3}>{(data) => <p>{data.text}</p>}</Await>
+      </React.Suspense>
+
+      <React.Suspense fallback={<p>loading (error)...</p>}>
+        <Await resolve={data.lazyError} errorElement={<RenderAwaitedError />}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App, { loader } from "./App";
+import "./index.css";
 import { worker } from "./mocks/browser";
 
 if (process.env.NODE_ENV === "development") {
   worker.start().catch((e) => console.log(e));
 }
 
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <App />,
+    loader,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
